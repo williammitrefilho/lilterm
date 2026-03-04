4f4a886f30fce0504cda32b000e18148e4121e75
williammitrefilho <willmitre@gmail.com>
2026-03-04 10:33:13 -0300

    Two classes, one for stashing and one for inputting

--- lilterm.js
+++ lilterm.js
@@ -1,9 +1,68 @@
 class LilTerm{
 	inputs = []
-	inputLine = new LilInputLine()
+	inputLine = new LilInput()
+	div = document.createElement("div")
+	historyDiv = document.createElement("div")
+	inputDiv = document.createElement("div")
 	constructor(){
 		this.inputLine.gotInput = (input)=>{
 			console.log(input)
-			this.lines.push(input)
+			this.inputs.unshift(input)
+			this.updateHistory()
+		}
+		this.setLineHeight(LilTerm.defaultLineHeight)
+		this.div.append(this.historyDiv, this.inputDiv)
+		this.inputDiv.appendChild(this.inputLine.p)
+		this.updateHistory()
+	}
+	setLineHeight(nLines){
+		this.lineHeight = nLines
+		while(this.inputs.length < nLines)
+			this.inputs.unshift({raw:">"})
+	}
+	updateHistory(){
+		this.historyDiv.innerHTML = this.inputs.slice(0, this.lineHeight)
+			.reverse()
+			.map(input=>`<p>${input.raw}</p>`)
+			.join("")
+	}
+	focus(){
+		this.inputLine.userSpace.click()
+	}
+}
+
+LilTerm.defaultLineHeight = 4
+
+class LilInput{
+	p = document.createElement("p")
+	userSpace = document.createElement("span")
+
+	constructor(){
+		this.p.innerHTML = ">"
+		this.userSpace.setAttribute("contenteditable", "true")
+		this.p.appendChild(this.userSpace)
+		this.p.onkeydown = (e)=>{
+			this.keyDown(e)
 		}
 	}
+	keyDown(e){
+		if(e.key == "Enter"){
+			e.preventDefault()
+			this.keyENTERPressed(e)
+		}
+	}
+	keyENTERPressed(e){
+		let rawInput = this.userSpace.innerHTML
+		this.gotInput(this.parse(rawInput))
+		this.userSpace.innerHTML = ""
+	}
+	parse(rawInput){
+		let parsed = {raw:rawInput},
+			parts = rawInput.split(" ")
+
+		parsed.command = parts.shift()
+		parsed.args = parts
+		return parsed
+	}
+	gotInput(){}
+}
